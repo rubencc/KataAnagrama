e00c59b4a7d101ae3518e0aaf457ce22f872affa
rubencc <rubencc@gmail.com>
2015-11-08 12:04:55 +0100

    Programa de consola

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,6 +10,18 @@ namespace Console
     {
         static void Main(string[] args)
         {
+            string input;
+
+            System.Console.WriteLine("Introduce una palabra");
+
+            input = System.Console.ReadLine();
+
+            System.Console.Clear();
+
+            System.Console.WriteLine("Resultados");
+            System.Console.WriteLine(String.Empty);
+
+            System.Console.Read();
         }
     }
 }
