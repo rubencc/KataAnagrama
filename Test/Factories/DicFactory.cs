4ea286dc35548d3b59038588cfddc28e8c92f824
rubencc <rubencc@gmail.com>
2015-11-10 11:25:23 +0100

    Añadida factoria

---
+++ Test/Factories/DicFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Business.Reader;
+
+namespace Test.Factories
+{
+    public class DicFactory
+    {
+        public static List<string> GetDic()
+        {
+            FileReader reader = new FileReader("dic.txt");
+
+            return reader.Load();
+        }
+    }
+}
