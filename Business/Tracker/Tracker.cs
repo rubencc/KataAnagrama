bf3e2e80c63e42261e2fc9e959d08df26c46cd34
rubencc <rubencc@gmail.com>
2015-11-11 09:42:40 +0100

    Añadido tracker

---
+++ Business/Tracker/Tracker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UserManagment;
+
+namespace Business.Tracker
+{
+    public class Tracker : IAnagramEngine
+    {
+        private readonly IAnagramEngine root;
+        private readonly List<string> interestingWords;
+
+        public Tracker(IAnagramEngine root, List<string> interestingWords)
+        {
+            if (root == null || interestingWords == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.root = root;
+            this.interestingWords = interestingWords;
+        }
+
+        public List<string> GetAnagrams(string input, List<string> words)
+        {
+            List<string> result = this.root.GetAnagrams(input, words);
+
+            this.AnalyzeQuery(input, result);
+
+            return result;
+        }
+
+        public List<string> GetContains(string input, List<string> words)
+        {
+            List<string> result = this.root.GetContains(input, words);
+            this.AnalyzeQuery(input, result);
+
+            return result;
+        }
+
+        public List<string> GetContainsCharsInOrder(string input, List<string> words)
+        {
+            List<string> result = this.root.GetContainsCharsInOrder(input, words);
+
+            this.AnalyzeQuery(input, result);
+
+            return result;
+        }
+
+        public List<string> GetContainsChars(string input, List<string> words)
+        {
+            List<string> result = this.root.GetContainsChars(input, words);
+
+            this.AnalyzeQuery(input, result);
+
+            return result;
+        }
+
+        private void AnalyzeQuery(string input, List<string> result)
+        {
+            if (this.ContainsWords(input, result))
+            {
+                this.TrackQuery(input);
+            }
+        }
+
+        private bool ContainsWords(string input, List<string> result)
+        {
+            return this.interestingWords.Contains(input) || this.interestingWords.FindAll(result.Contains).Count > 0;
+        }
+
+        private void TrackQuery(string input)
+        {
+            IUserInfo userInfo = UserInfo.Instance;
+
+            Console.WriteLine("Word: {4} --- IP: {0}, Country: {1}, Language: {2}, SearchTime: {3}", userInfo.Ip, userInfo.Country, userInfo.Language, userInfo.Time, input);
+        }
+    }
+}
--- UserManagment/UserInfo.cs
+++ UserManagment/UserInfo.cs
@@ -8,6 +8,22 @@ namespace UserManagment
     public sealed class UserInfo : IUserInfo
     {
 
+        private static UserInfo instance;
+
+        private UserInfo() { }
+
+        public static UserInfo Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserInfo();
+                }
+                return instance;
+            }
+        }
+
         public DateTime Time
         {
             get { return this.GetDate(); }
@@ -20,7 +36,7 @@ namespace UserManagment
 
         public string Language
         {
-            get { return this.GetLanguage();}
+            get { return this.GetLanguage(); }
         }
 
         public string Ip
