cde77ee63fb1a6bfd865911e5209614c6adc680b
rubencc <rubencc@gmail.com>
2015-11-11 10:11:44 +0100

    Añadido logger

---
+++ Business/Log/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace Business.Log
+{
+    public interface ILogger
+    {
+        void Log(string toLog);
+    }
+}
---
+++ Business/Log/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Business.Log
+{
+    public class Logger : ILogger
+    {
+        public void Log(string toLog)
+        {
+            Console.WriteLine(toLog);
+        }
+    }
+}
