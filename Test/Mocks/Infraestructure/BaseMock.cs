1aeae7b155037e6750eca6c877d675cf45558a66
rubencc <rubencc@gmail.com>
2015-11-12 12:53:39 +0100

    Factoria de mocks

---
+++ Test/Mocks/Infraestructure/BaseMock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Moq;
+using Moq.Language.Flow;
+
+namespace Test.Mocks.Infraestructure
+{
+    public abstract class BaseMock<T> where T : class
+    {
+        protected Mock<T> objectMock;
+
+        public BaseMock()
+        {
+           this.objectMock = new Mock<T>();
+        }
+
+        public T Mock
+        {
+            get { return this.objectMock.Object; }
+        }
+
+        public ISetup<T, List<string>> Setup(Expression<Func<T, List<string>>> expression)
+        {
+            return this.objectMock.Setup(expression);
+        }
+
+        public ISetup<T> Setup(Expression<Action<T>> expression)
+        {
+            return this.objectMock.Setup(expression);
+        }
+
+        public void Verify(Expression<Func<T, List<string>>> expression, Times times)
+        {
+            this.objectMock.Verify(expression, times);
+        }
+
+        public void Verify(Expression<Func<T, List<string>>> expression, Times times, string failMessage)
+        {
+            this.objectMock.Verify(expression, times, failMessage);
+        }
+
+        public void Verify(Expression<Action<T>> expression, Times times)
+        {
+            this.objectMock.Verify(expression, times);
+        }
+
+        public void Verify(Expression<Action<T>> expression, Times times, string failMessage)
+        {
+            this.objectMock.Verify(expression, times, failMessage);
+        }
+    }
+}
---
+++ Test/Mocks/Infraestructure/FactoryMock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test.Mocks.Infraestructure
+{
+    public class FactoryMock
+    {
+        public static MockObject<T> CreateMock<T>() where T: class
+        {
+            return new MockObject<T>();
+        }
+
+        public static T Get<T>()
+        {
+            return (T)Activator.CreateInstance(typeof(T));
+        }
+    }
+}
